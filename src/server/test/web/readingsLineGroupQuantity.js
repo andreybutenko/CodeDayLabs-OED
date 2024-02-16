4422b1b27deaccbd1eb190be76ccd96d54690f12
Andrey Butenko <andreyxbutenko@gmail.com>
2024-02-16 14:52:08 -0800

    Add progress from feb 16

--- src/server/test/web/readingsLineGroupQuantity.js
+++ src/server/test/web/readingsLineGroupQuantity.js
@@ -57,14 +57,18 @@ mocha.describe('readings API', () => {
 
                 // Add LG11 here
 
-                // Add LG12 here
-                mocha.it('LG12', async () => {
+                mocha.it('LG12: should have daily points for 15 + 20 minute reading intervals and quantity units with +-inf start/end time & kWh as BTU chained', async () => {
                     // Load the data into the database
                     await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWhGroups, groupDatakWh);
                     // Get the unit ID since the DB could use any value.
                     const unitId = await getUnitId('BTU');
                     // Load the expected response data from the corresponding csv file
                     const expected = await parseExpectedCsv('src/server/test/web/readingsData/expected_line_group_ri_15-20_mu_kWh_gu_BTU_st_-inf_et_inf.csv');
+                    // Create a request to the API for unbounded reading times and save the response
+                    const res = await chai.request(app).get(`/api/unitReadings/line/groups/${GROUP_ID}`)
+                        .query({ timeInterval: ETERNITY.toString(), graphicUnitId: unitId });
+                    // Check that the API reading is equal to what it is expected to equal
+                    expectReadingToEqualExpected(res, expected, GROUP_ID);
                 });
 
                 // Add LG13 here
